02a0e2203d9e227546870614f032bb5aab9bf93f
Owen <owen.poizac@gmail.com>
2025-07-16 16:26:08 +0200

    simplification_du_code

--- password_generator.py
+++ password_generator.py
@@ -1,39 +1,19 @@
 import random
 import string
 
-chars = string.ascii_letters + string.digits + string.punctuation
-
-
-def generate_password(length, uppercase, digits, symbol):
+def generate_password(length, use_upper, use_digits, use_symbols):
     chars = string.ascii_lowercase
-    if uppercase:
-        chars += string.ascii_uppercase
-    if digits:
-        chars += string.digits
-    if symbol:
-        chars += string.punctuation
-    return "".join(random.choice(chars) for _ in range(lenght))
-
+    if use_upper: chars += string.ascii_uppercase
+    if use_digits: chars += string.digits
+    if use_symbols: chars += string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
 
 def word_processing(word):
     return word.encode("unicode_escape").decode()
 
-
 if __name__ == "__main__":
-    lenght: int = int(input("Entrez la longueur du mot de passe : "))
-    uppercase = input("Voulez vous des majuscules ? (o/n)")
-    if uppercase == "o":
-        uppercase = True
-    else:
-        uppercase = False
-    digits = input("Voulez vous des chiffres ? (o/n)")
-    if digits == "o":
-        digits = True
-    else:
-        digits = False
-    symbol = input("Voulez vous des symboles ? (o/n)")
-    if symbol == "o":
-        symbol = True
-    else:
-        symbol = False
-    print(word_processing(generate_password(int(lenght), uppercase, digits, symbol)))
+    length = int(input("Longueur du mot de passe : "))
+    upper = input("Majuscules ? (o/n) : ").lower() == "o"
+    digits = input("Chiffres ? (o/n) : ").lower() == "o"
+    symbols = input("Symboles ? (o/n) : ").lower() == "o"
+    print(word_processing(generate_password(length, upper, digits, symbols)))
